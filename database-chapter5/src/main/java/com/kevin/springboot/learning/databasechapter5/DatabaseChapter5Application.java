abd326ab2d32df62b4527217d4e95d399db56f1a
yuanzhongkai <zkyspace@163.com>
2018-12-16 15:05:53 +0800

    chapter5 init commit

---
+++ database-chapter5/src/main/java/com/kevin/springboot/learning/databasechapter5/DatabaseChapter5Application.java
@@ -0,0 +1,14 @@
+package com.kevin.springboot.learning.databasechapter5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseChapter5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseChapter5Application.class, args);
+	}
+
+}
+
