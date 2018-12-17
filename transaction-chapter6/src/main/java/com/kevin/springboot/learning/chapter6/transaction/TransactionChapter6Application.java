ae52e44ab601db6973e14adc87a7bee19e5cc3bb
yzk <zkyspace@163.com>
2018-12-17 20:58:29 +0800

    transaction-chapter6 init commit

---
+++ transaction-chapter6/src/main/java/com/kevin/springboot/learning/chapter6/transaction/TransactionChapter6Application.java
@@ -0,0 +1,14 @@
+package com.kevin.springboot.learning.chapter6.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionChapter6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionChapter6Application.class, args);
+	}
+
+}
+
