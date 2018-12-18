d4e968b1731cb8bed49f6a7a91d76c136f2d5679
yuanzhongkai <zkyspace@163.com>
2018-12-18 10:42:57 +0800

    added common redisTemplate options

--- redis-chapter7/src/test/java/com/kevin/springboot/learning/chapter7/redis/RedisApplicationTests.java
+++ redis-chapter7/src/test/java/com/kevin/springboot/learning/chapter7/redis/RedisApplicationTests.java
@@ -1,16 +1,26 @@
 package com.kevin.springboot.learning.chapter7.redis;
 
+import com.kevin.springboot.learning.chapter7.redis.config.RedisConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RedisApplicationTests {
-
+	private static Logger logger = LoggerFactory.getLogger(RedisApplicationTests.class);
 	@Test
 	public void contextLoads() {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(RedisConfig.class);
+		RedisTemplate redisTemplate = applicationContext.getBean(RedisTemplate.class);
+		redisTemplate.opsForValue().set("gqq", "shabi");
+		Object gqq = redisTemplate.opsForValue().get("gqq");
+		logger.info("gqq from redis: {}", gqq);
 	}
 
 }
