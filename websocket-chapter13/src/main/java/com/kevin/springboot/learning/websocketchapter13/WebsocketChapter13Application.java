6e459150d154f43a23f73029243996997a3acc41
yuanzhongkai <zkyspace@163.com>
2018-12-20 15:30:53 +0800

    websocket init commit

---
+++ websocket-chapter13/src/main/java/com/kevin/springboot/learning/websocketchapter13/WebsocketChapter13Application.java
@@ -0,0 +1,14 @@
+package com.kevin.springboot.learning.websocketchapter13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketChapter13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketChapter13Application.class, args);
+	}
+
+}
+
